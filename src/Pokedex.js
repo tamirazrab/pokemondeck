9486a55099e7a171230d7461e7923f55ffc4ff55
Tamir Azrab <tamirazrab@gmail.com>
2020-11-12 20:15:45 +0500

    Finished: Styling winner and loser

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,27 +3,26 @@ import Pokecard from "./Pokecard";
 import "./Pokedex.css";
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-    ],
-  };
-
   render() {
+    let title, experience;
+    if (this.props.isWinner) {
+      title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+      experience = <p>
+        Total Experience : <strong>{this.props.exp}</strong>
+      </p>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+      experience =  <p>
+        Total Experience : {this.props.exp}
+      </p>;
+    }
     return (
       <div className="Pokedex">
         <h1>Pokedex</h1>
-        <p> Total Experience : {this.props.exp} </p>
-        <p> {this.props.isWinner ? 'Winner' : 'Loser'}</p>
+        {/* no need to add p tag along with experience and title but just added anyway */}
+        <p> {experience} </p>
+        <p> {title} </p>
         <div className="Pokedex-card">
-          
           {this.props.pokemon.map((p) => (
             <Pokecard
               id={p.id}
