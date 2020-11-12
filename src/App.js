8fc96a3d86841e3e0531e4258f89bd21c9ab646f
Tamir Azrab <tamirazrab@gmail.com>
2020-11-12 19:37:46 +0500

    Added: Pokegame component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 
 
 function App() {
   return (
     <div className="App">
-      <Pokedex /> 
+     <Pokegame /> 
     </div>
   );
 }
---
+++ src/Pokegame.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Pokedex from './Pokedex';
+
+class Pokegame extends Component {
+
+    static defaultProps = {
+        pokemon: [
+            { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+            { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+            { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+            { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+            { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+            { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+            { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+            { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+        ],
+    };
+    render() {
+        let firstDeckHand = [];
+        let secondDeckHand = [ ...this.props.pokemon];
+        // const pokemonList = [...this.props.pokemon];
+        while( firstDeckHand.length < secondDeckHand.length )   {
+            let randomPokemon = Math.floor(Math.random() * secondDeckHand.length);
+            let selectedRandomPokemon = secondDeckHand.splice(randomPokemon, 1)[0];
+            // index at last represents index of object which returned by splice at index 0 it contains object information, in this case data of pokemon
+
+            firstDeckHand.push(selectedRandomPokemon);
+        }
+        
+        return (
+           <div>
+               <Pokedex pokemon={firstDeckHand} />
+               <Pokedex pokemon={secondDeckHand} />
+           </div> 
+        );
+    }
+}
+
+export default Pokegame;
