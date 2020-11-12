737a41238cf11b383d38af00dba1e4411f0420d1
Tamir Azrab <tamirazrab@gmail.com>
2020-11-12 18:14:54 +0500

    Initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Pokecard from './Pokecard';
+function App() {
+  return (
+    <div className="App">
+      <Pokecard /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pokecard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Pokecard extends Component {
+    render() {
+        return <h1>Hello.</h1>
+    }
+
+}
+
+export default Pokecard;
